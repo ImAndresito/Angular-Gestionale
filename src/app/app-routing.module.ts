26dfc741b8bacd5a0b1d109e0b00c67595103e14
Andres <andres.villasanchez@erre.company>
2024-01-10 10:42:29 +0100

    cards and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DipendentiComponent } from './dashboard/business-unit/dipendenti/dipendenti.component';
+import { BusinessUnitComponent } from './dashboard/business-unit/business-unit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BusinessUnitComponent,
+  },
+  {
+    path: 'dipendenti',
+    component: DipendentiComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
