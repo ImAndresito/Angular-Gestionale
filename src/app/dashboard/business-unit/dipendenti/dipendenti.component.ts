8d0651140adc667dccee56b30ee8919d31377c36
Andres <andresito.villa.01@gmail.com>
2024-01-10 03:05:07 +0100

    Responsive Vers.1.0.0

---
+++ src/app/dashboard/business-unit/dipendenti/dipendenti.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dipendenti',
+  templateUrl: './dipendenti.component.html',
+  styleUrls: ['./dipendenti.component.css']
+})
+export class DipendentiComponent {
+
+}
